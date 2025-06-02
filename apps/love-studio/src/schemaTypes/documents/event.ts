5d587f92c3cefb3ff6deefe44e8196070511934f
Raúl Garcia <ego.war@icloud.com>
2025-06-01 23:00:22 -0700

    feat(event): update schema

--- apps/love-studio/src/schemaTypes/documents/event.ts
+++ apps/love-studio/src/schemaTypes/documents/event.ts
@@ -9,5 +9,44 @@ export const event = defineType({
       name: 'name',
       type: 'string',
     }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+    }),
+    defineField({
+      name: 'eventType',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'doorsOpen',
+      type: 'number',
+    }),
+    defineField({
+      name: 'venue',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'venue'}]}],
+    }),
+    defineField({
+      name: 'artists',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'artist'}]}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
   ],
 })
