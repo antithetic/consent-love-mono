612068933277eeb28400a962d937aa8db475b2de
Raúl Garcia <ego.war@icloud.com>
2025-06-01 20:12:02 -0700

    update eslint, move schema to /src

--- love-studio/sanity.cli.ts
+++ love-studio/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '6ziojjbn',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
