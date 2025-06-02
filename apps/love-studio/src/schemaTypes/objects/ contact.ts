13e238e2a1582a53ac1907f3d04d778f46524135
Raúl Garcia <ego.war@icloud.com>
2025-06-02 02:44:59 -0700

    feat(init): yay

--- apps/love-studio/src/schemaTypes/objects/ contact.ts
+++ apps/love-studio/src/schemaTypes/objects/ contact.ts
@@ -1,1 +1,13 @@
 import {defineField, defineType} from 'sanity'
+export const contact = defineType({
+  name: 'contact',
+  type: 'document',
+  title: 'Contact',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Contact Name',
+    }),
+  ],
+})
