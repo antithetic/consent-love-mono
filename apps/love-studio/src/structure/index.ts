1aef3372e780b83b13174401405e1c96b7e0a2b7
Raúl Garcia <ego.war@icloud.com>
2025-06-02 11:40:43 -0700

    feat(structure): upcomming/past events

--- apps/love-studio/src/structure/index.ts
+++ apps/love-studio/src/structure/index.ts
@@ -10,7 +10,19 @@ export const structure: StructureResolver = (S) =>
     .id('root')
     .title('Content')
     .items([
-      S.documentTypeListItem('event').title('Events').icon(CalendarDotsIcon),
+      S.listItem()
+        .title('Upcoming Events')
+        .schemaType('event')
+        .icon(CalendarDotsIcon)
+        .child(
+          S.documentList().title('Upcoming Events').filter('date >= now()'),
+        ),
+      S.listItem()
+        .title('Past Events')
+        .schemaType('event')
+        .icon(CalendarDotsIcon)
+        .child(S.documentList().title('Past Events').filter('date < now()')),
+      S.divider(),
       S.documentTypeListItem('artist').title('Artists').icon(UserRectangleIcon),
       S.documentTypeListItem('venue').title('Venues').icon(FactoryIcon),
     ])
