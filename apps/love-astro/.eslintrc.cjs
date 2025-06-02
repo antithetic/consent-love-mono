41a9ce57e8b57937df320531934aaca1eaf4a6fc
Raúl Garcia <ego.war@icloud.com>
2025-06-02 16:45:38 -0700

    hotfix(eslint): working lint, but with warnings

---
+++ apps/love-astro/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      // Only add basic TypeScript rules without extending the full config
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+  ],
+  rules: {
+    // Your custom rules here
+  },
+};
