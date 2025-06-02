30739efb803334d1cfa08d1d6ea22bf15a89277a
Raúl Garcia <ego.war@icloud.com>
2025-06-02 01:27:51 -0700

    feat(event): add contact referennces

--- apps/love-studio/src/schemaTypes/documents/contact.ts
+++ apps/love-studio/src/schemaTypes/documents/contact.ts
@@ -8,5 +8,22 @@ export const contact = defineType({
       name: 'name',
       type: 'string',
     }),
+    defineField({
+      name: 'email',
+      type: 'email',
+      title: 'Contact Email',
+    }),
+    defineField({
+      name: 'info',
+      title: 'Contact notes',
+      type: 'text',
+      rows: 3,
+    }),
+    defineField({
+      name: 'connections',
+      title: 'Contact Connections',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'contact'}]}],
+    }),
   ],
 })
