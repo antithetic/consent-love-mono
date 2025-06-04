fb0be50ac21e19722daab0d583abfe2da834a6b4
Raúl Garcia <ego.war@icloud.com>
2025-06-03 20:30:28 -0700

    feat(layout): update css

--- apps/love-astro/.prettierrc.mjs
+++ apps/love-astro/.prettierrc.mjs
@@ -1,15 +1,15 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   singleQuote: true,
   printWidth: 80,
-  tailwindStylesheet: "@styles/global.css",
+  tailwindStylesheet: '@styles/global.css',
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
--- apps/love-astro/src/consts.ts
+++ apps/love-astro/src/consts.ts
@@ -1,32 +1,31 @@
-import type { Metadata, Site, Socials } from "@/types";
+import type { Metadata, Site, Socials } from '@/types';
 
 export const SITE: Site = {
-  TITLE: "LOVE HANGOVER",
-  DESCRIPTION: "QUEER. PUNK. ACID. SWEAT.",
-  EMAIL: "love@consent.press",
+  TITLE: 'LOVE HANGOVER',
+  DESCRIPTION: 'QUEER. PUNK. ACID. SWEAT.',
+  EMAIL: 'love@consent.press',
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "HOME",
+  TITLE: 'Home',
+  DESCRIPTION: 'HOME',
 };
 
 export const ABOUT: Metadata = {
-  TITLE: "About",
-  DESCRIPTION: "ABOUT",
+  TITLE: 'About',
+  DESCRIPTION: 'ABOUT',
 };
 
 export const EVENTS: Metadata = {
-  TITLE: "Events",
-  DESCRIPTION:
-    "EVENTS",
+  TITLE: 'Events',
+  DESCRIPTION: 'EVENTS',
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: "Instagram",
-    HREF: "https://instagram.com/love.hangover.sd",
+    NAME: 'Instagram',
+    HREF: 'https://instagram.com/love.hangover.sd',
   },
 ];
