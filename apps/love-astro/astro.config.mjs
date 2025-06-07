87f99ab28b9535e34a126fe2c37ca11fb7fe3db6
Raúl Garcia <ego.war@icloud.com>
2025-06-06 22:43:28 -0700

    chore(deps): add astro compressor

--- apps/love-astro/astro.config.mjs
+++ apps/love-astro/astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [compressor({
+    brotli: true,
+    gzip: false,
+  })],
 })
